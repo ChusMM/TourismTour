bfe2bb1af31a7b0656e26e7361e1af510ccf72eb
chuster <jesus.munoz.lanaccess@gmail.com>
2014-05-18 17:20:54 +0200

    Commit test

--- src/com/tourism/map/BluetoothOutput.java
+++ src/com/tourism/map/BluetoothOutput.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-//import java.util.Set;
+//import java.util.Set; 
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
