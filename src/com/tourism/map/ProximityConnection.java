fbc6547db6de35e47ed05fc4b5a92c11b80f0ec6
odvn <oscar.delvalle85@gmail.com>
2014-10-25 18:36:29 +0000

    Nueva url de la bbdd

--- src/com/tourism/map/ProximityConnection.java
+++ src/com/tourism/map/ProximityConnection.java
@@ -11,44 +11,56 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-import com.tourism.map.exceptions.ProximityLoadException;
+import com.tourism.map.exceptions.ServerConnectionException;
 
 import android.os.AsyncTask;
 import android.util.Log;
-public class ProximityConnection extends AsyncTask<Void, Void, String> {
-	private static final String TAG = "com.tourism.map.ProximityConnection";
+
+public class ServerConnection extends AsyncTask<String,String,String>{
+	private static final String TAG = "com.tourism.map.ServerConnection";
+	private static final String URL = "http://tourapp.esy.es//main.php";
 	
-	private static final String URL = "http://tourapp.zz.mu//coordinates.php";
-	private ProximityLoadException mProxException = new ProximityLoadException("Fatal unexpected Proximity exception");
+	private float latitude;
+	private float longuitude;
+	private ServerConnectionException mServException = new ServerConnectionException("Fatal unexpected Proximity exception");
 	
-	public ProximityLoadException getProxException() {
-		return this.mProxException;
+	public ServerConnection(float lat, float lon) {
+		this.latitude = lat;
+		this.longuitude = lon;
+	}
+	
+	public ServerConnectionException getServException() {
+		return this.mServException;
 	}
 	
 	@Override
-	protected String doInBackground(Void... args) {
+	protected String doInBackground(String... params) {
 		try {
 			ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			nameValuePairs.add(new BasicNameValuePair("PosY", (Float.valueOf(latitude)).toString()));
+			nameValuePairs.add(new BasicNameValuePair("PosX", (Float.valueOf(longuitude)).toString()));
+
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpPost httppost = new HttpPost(URL);
 			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			
+
 			HttpResponse response = httpclient.execute(httppost);
 			HttpEntity entity = response.getEntity();
-			return EntityUtils.toString(entity, "ISO-8859-1");
+			return EntityUtils.toString(entity);
 		} catch (ClientProtocolException e) {
 			Log.e(TAG, e.toString());
-			mProxException = new ProximityLoadException(e.getMessage());
+			mServException = new ServerConnectionException(e.getMessage());
 			return null;
 		} catch (IOException e) {
 			Log.e(TAG, e.toString());
-			mProxException = new ProximityLoadException(e.getMessage());
+			mServException = new ServerConnectionException(e.getMessage());
 			return null;
 		} catch (Exception e) {
 			Log.e(TAG, e.toString());
-			mProxException = new ProximityLoadException(e.getMessage());
+			mServException = new ServerConnectionException(e.getMessage());
 			return null;
 		}
 	}
